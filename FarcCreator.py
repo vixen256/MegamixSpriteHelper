067bc49774b8f56b98ee3c0dacaa1f37cbc9fd1f
vixen256 <64546358+vixen256@users.noreply.github.com>
2026-01-03 11:58:51 +1300

    Support kkdlib-sys with gpu bcn encoding

--- FarcCreator.py
+++ FarcCreator.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 
-import KKdLib
+import kkdlib
 
 class Compression(Enum):
     ATI2 = "YCbCr"
@@ -11,77 +11,98 @@ class Compression(Enum):
     def __str__(self):
         return f"{self.value}"
 
+    def to_kkdlib_format(self):
+        match self:
+            case Compression.ATI2:
+               return kkdlib.txp.Format.BC5
+            case Compression.DXT5:
+                return kkdlib.txp.Format.BC3
+            case Compression.BC7:
+                return kkdlib.txp.Format.BC7
+            case Compression.RGBA:
+                return kkdlib.txp.Format.RGBA
+
+
 class FarcCreator:
     def create_jk_bg_logo_farc(self,song_id,jk_bg_texture,logo_texture,output_location,compression:Compression):
 
-        txp = KKdLib.txp_set()
-        txp.add_texture_pillow("SH Texture #1",jk_bg_texture,compression.name)
+        txp = kkdlib.txp.Set()
+        names = ["SH Texture #1"]
+        if compression is Compression.ATI2:
+            txp.add_file(kkdlib.txp.Texture.py_ycbcr_from_rgba_gpu(jk_bg_texture.width,jk_bg_texture.height,jk_bg_texture.tobytes()))
+        else:
+            txp.add_file(kkdlib.txp.Texture.py_from_rgba_gpu(jk_bg_texture.width,jk_bg_texture.height,jk_bg_texture.tobytes(),compression.to_kkdlib_format()))
 
-        background = KKdLib.sprite_info()
-        background.name = str("SONG_BG" + song_id)
-        background.texid = txp.get_texture_id("SH Texture #1")
-        background.resolution_mode = "FHD"
-        background.x = 2
-        background.y = 2
+        background = kkdlib.spr.Info()
+        background.texid = 0
+        background.resolution_mode = kkdlib.spr.ResolutionMode.FHD
+        background.px = 2
+        background.py = 2
         background.width = 1280
         background.height = 720
 
-        jacket = KKdLib.sprite_info()
-        jacket.name = str("SONG_JK" + song_id)
-        jacket.texid = txp.get_texture_id("SH Texture #1")
-        jacket.resolution_mode = "FHD"
-        jacket.x = 1286
-        jacket.y = 2
+        jacket = kkdlib.spr.Info()
+        jacket.texid = 0
+        jacket.resolution_mode = kkdlib.spr.ResolutionMode.FHD
+        jacket.px = 1286
+        jacket.py = 2
         jacket.width = 502
         jacket.height = 502
 
-        spr = KKdLib.spr_set()
+        spr = kkdlib.spr.Set()
 
 
         if logo_texture is not None:
-            txp.add_texture_pillow("SH Texture #2",logo_texture,compression.name)
+            names.append("SH Texture #2")
+            if compression is Compression.ATI2:
+                txp.add_file(kkdlib.txp.Texture.py_ycbcr_from_rgba_gpu(logo_texture.width,logo_texture.height,logo_texture.tobytes()))
+            else:
+                txp.add_file(kkdlib.txp.Texture.py_from_rgba_gpu(logo_texture.width,logo_texture.height,logo_texture.tobytes(),compression.to_kkdlib_format()))
 
-            logo = KKdLib.sprite_info()
-            logo.name = str("SONG_LOGO" + song_id)
-            logo.texid = txp.get_texture_id("SH Texture #2")
-            logo.resolution_mode = "FHD"
-            logo.x = 2
-            logo.y = 2
+            logo = kkdlib.spr.Info()
+            logo.texid = 1
+            logo.resolution_mode = kkdlib.spr.ResolutionMode.FHD
+            logo.px = 2
+            logo.py = 2
             logo.width = 870
             logo.height = 330
 
 
 
-        spr.txp = txp
-        spr.add_sprite(background)
-        spr.add_sprite(jacket)
+        spr.set_txp(txp, names)
+        spr.ready = True
+        spr.add_spr(background, str("SONG_BG" + song_id))
+        spr.add_spr(jacket, str("SONG_JK" + song_id))
         if logo_texture is not None:
-            spr.add_sprite(logo)
+            spr.add_spr(logo, str("SONG_LOGO" + song_id))
 
-        farc = KKdLib.farc()
-        farc.add_file(KKdLib.farc_file(name="spr_sel_pv"+song_id+".bin", data=spr.pack()))
-        farc.write(output_location+'/spr_sel_pv'+song_id+'.farc')
+        farc = kkdlib.farc.Farc()
+        farc.add_file_data("spr_sel_pv"+song_id+".bin", spr.to_buf())
+        farc.write(output_location+'/spr_sel_pv'+song_id+'.farc', False, False)
 
     def create_thumbnail_farc(self,thumb_data,thumbnail_texture,output_location,mod_name,compression:Compression):
-        txp = KKdLib.txp_set()
-        txp.add_texture_pillow("SH Texture #1", thumbnail_texture,compression.name)
+        txp = kkdlib.txp.Set()
+        if compression is Compression.ATI2:
+            txp.add_file(kkdlib.txp.Texture.py_ycbcr_from_rgba_gpu(thumbnail_texture.width,thumbnail_texture.height,thumbnail_texture.tobytes()))
+        else:
+            txp.add_file(kkdlib.txp.Texture.py_from_rgba_gpu(thumbnail_texture.width,thumbnail_texture.height,thumbnail_texture.tobytes(),compression.to_kkdlib_format()))
 
-        spr = KKdLib.spr_set()
-        spr.txp = txp
+        spr = kkdlib.spr.Set()
+        spr.set_txp(txp, ["SH Texture #1"])
+        spr.ready = True
 
         for thumb in thumb_data:
             #[id,(x,y)]
-            thumbnail = KKdLib.sprite_info()
-            thumbnail.name = thumb[0]
-            thumbnail.texid = txp.get_texture_id("SH Texture #1")
-            thumbnail.resolution_mode = "FHD"
-            thumbnail.x = thumb[1][0]
-            thumbnail.y = thumb[1][1]
+            thumbnail = kkdlib.spr.Info()
+            thumbnail.texid = 0
+            thumbnail.resolution_mode = kkdlib.spr.ResolutionMode.FHD
+            thumbnail.px = thumb[1][0]
+            thumbnail.py = thumb[1][1]
             thumbnail.width = 128
             thumbnail.height = 64
 
-            spr.add_sprite(thumbnail)
+            spr.add_spr(thumbnail, thumb[0])
 
-        farc = KKdLib.farc()
-        farc.add_file(KKdLib.farc_file(name="spr_sel_pvtmb_"+mod_name+".bin", data=spr.pack()))
-        farc.write(output_location + '/spr_sel_pvtmb_' + mod_name + '.farc')
+        farc = kkdlib.farc.Farc()
+        farc.add_file_data("spr_sel_pvtmb_"+mod_name+".bin", spr.to_buf())
+        farc.write(output_location + '/spr_sel_pvtmb_' + mod_name + '.farc', False, False)
